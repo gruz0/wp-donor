afbf57c67fabf98fa751b125751f4e35884ec22d
Alexander Gruzov <gruz0.mail@gmail.com>
2016-06-05 14:42:06 +0300

    Return status code 1 if settings are not valid
    	modified:   load-settings.php

--- load-settings.php
+++ load-settings.php
@@ -1,41 +1,49 @@
 <?php
 
 if ( ! file_exists( APP_PATH . 'settings.php' ) || ! is_readable( APP_PATH . 'settings.php' ) ) {
-	die( 'settings.php is not exists or readable' );
+	echo 'settings.php is not exists or readable';
+	die(1);
 }
 
 require_once( APP_PATH . 'settings.php' );
 
 if ( empty( $settings ) || ! is_array( $settings ) ) {
-	die( 'Settings are not present!' );
+	echo 'Settings are not present!';
+	die(1);
 }
 
 if ( empty( $settings['donor_path'] ) || ! is_dir( $settings['donor_path'] ) || ! is_readable( $settings['donor_path'] ) ) {
-	die( 'donor_path is not present in settings or not exists or not readable' );
+	echo 'donor_path is not present in settings or not exists or not readable';
+	die(1);
 }
 
 if ( empty( $settings['acceptors'] ) || ! is_array( $settings['acceptors'] ) || count( $settings['acceptors'] ) == 0 ) {
-	die( 'acceptors are not present in settings or not an array' );
+	echo 'acceptors are not present in settings or not an array';
+	die(1);
 }
 
 foreach( $settings['acceptors'] as $acceptor_name => $acceptor_values ) {
 	if ( empty( $acceptor_name ) ) {
-		die( 'acceptor acceptor_name is empty' );
+		echo 'acceptor acceptor_name is empty';
+		die(1);
 	}
 
 	if ( empty( $acceptor_values ) || ! is_array( $acceptor_values ) || count( $acceptor_values ) == 0 ) {
-		die( 'acceptors are not present in settings or not an array' );
+		echo 'acceptors are not present in settings or not an array';
+		die(1);
 	}
 
 	foreach( $acceptor_values as $key => $value ) {
 		if ( ! is_string( $key ) ) {
-			die( 'acceptor key is not present a string' );
+			echo 'acceptor key is not present a string';
+			die(1);
 		}
 
 		switch ( $key ) {
 			case 'path':
 				if ( empty( $value ) || ! is_dir( $value ) || ! is_readable( $value ) ) {
-					die( 'acceptor path is not present or not exists or not readable' );
+					echo 'acceptor path is not present or not exists or not readable';
+					die(1);
 				}
 				break;
 		}
