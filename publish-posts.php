7bc31edbb99697432ad20547d72f9519d0eb88ef
Alexander Gruzov <gruz0.mail@gmail.com>
2016-06-05 15:29:57 +0300

    Publish posts to acceptors
    	new file:   publish-posts.php

---
+++ publish-posts.php
@@ -0,0 +1,30 @@
+<?php
+error_reporting(E_ALL);
+
+define( 'APP_PATH', dirname( __FILE__ ) . DIRECTORY_SEPARATOR );
+define( 'WP_USE_THEMES', false );
+
+require_once( APP_PATH . 'load-settings.php' );
+
+$content = file_get_contents( APP_PATH . 'posts/posts-' . date("Ymd") . '.json' );
+$posts   = json_decode( $content );
+
+foreach( $settings['acceptors'] as $acceptor_sitename => $acceptor_settings ) {
+	require_once( $acceptor_settings['path'] . 'wp-load.php' );
+
+	foreach ( $posts as $post_idx => $post ) {
+
+		// Create post object
+		$new_post = array(
+		  'post_title'    => $post->title,
+		  'post_content'  => $post->content,
+		  'post_status'   => 'publish',
+		  'post_author'   => 1,
+		  // 'post_category' => array( 8,39 )
+		);
+
+		// Insert the post into the database
+		wp_insert_post( $new_post );
+	}
+}
+
