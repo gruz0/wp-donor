9823ccb50bd61ab054e608b3d2d49e8b57bca6fe
Alexander Gruzov <gruz0.mail@gmail.com>
2016-06-05 12:54:08 +0300

    Add settings validator

--- clone.php
+++ clone.php
@@ -11,6 +11,34 @@ if ( empty( $settings['donor_path'] ) || ! is_dir( $settings['donor_path'] ) || 
 	die( 'donor_path is not present in settings or not exists or not readable' );
 }
 
+if ( empty( $settings['acceptors'] ) || ! is_array( $settings['acceptors'] ) || count( $settings['acceptors'] ) == 0 ) {
+	die( 'acceptors are not present in settings or not an array' );
+}
+
+foreach( $settings['acceptors'] as $acceptor_name => $acceptor_values ) {
+	if ( empty( $acceptor_name ) ) {
+		die( 'acceptor acceptor_name is empty' );
+	}
+
+	if ( empty( $acceptor_values ) || ! is_array( $acceptor_values ) || count( $acceptor_values ) == 0 ) {
+		die( 'acceptors are not present in settings or not an array' );
+	}
+
+	foreach( $acceptor_values as $key => $value ) {
+		if ( ! is_string( $key ) ) {
+			die( 'acceptor key is not present a string' );
+		}
+
+		switch ( $key ) {
+			case 'path':
+				if ( empty( $value ) || ! is_dir( $value ) || ! is_readable( $value ) ) {
+					die( 'acceptor path is not present or not exists or not readable' );
+				}
+				break;
+		}
+	}
+}
+
 include_once( $settings['donor_path'] . 'wp-load.php' );
 
 // TODO: Получение и добавление категорий
