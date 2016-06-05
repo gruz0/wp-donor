2bcfc3397f8d9b53fd1a9509b7002a4e685fdbaf
Alexander Gruzov <gruz0.mail@gmail.com>
2016-06-05 15:30:50 +0300

    Prevent to publish post with duplicate title
    	modified:   publish-posts.php

--- publish-posts.php
+++ publish-posts.php
@@ -12,7 +12,21 @@ $posts   = json_decode( $content );
 foreach( $settings['acceptors'] as $acceptor_sitename => $acceptor_settings ) {
 	require_once( $acceptor_settings['path'] . 'wp-load.php' );
 
+	if ( ! empty( $acceptor_settings['allow_duplicate_post_title'] ) ) {
+		$allow_duplicate_post_title = absint( $acceptor_settings['allow_duplicate_post_title'] ) == 1;
+	} else {
+		$allow_duplicate_post_title = ALLOW_DUPLICATE_POST_TITLE;
+	}
+
 	foreach ( $posts as $post_idx => $post ) {
+
+		if ( ! $allow_duplicate_post_title ) {
+			$post_by_title = get_page_by_title( $post->title, OBJECT, 'post' );
+			if ( $post_by_title && $post_by_title->post_status == 'publish') {
+				echo "Skip post \"{$post->title}\" from publish because title is not unique...\n";
+				continue;
+			}
+		}
 
 		// Create post object
 		$new_post = array(
