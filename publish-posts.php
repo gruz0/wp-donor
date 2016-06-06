2ced78101b3b32cf190ab7709095a83634ba5111
Alexander Gruzov <gruz0.mail@gmail.com>
2016-06-06 11:10:58 +0300

    Extract code to create categories to single function create_missing_categories()
    	modified:   publish-posts.php

--- publish-posts.php
+++ publish-posts.php
@@ -29,11 +29,14 @@ foreach( $settings['acceptors'] as $acceptor_sitename => $acceptor_settings ) {
 	// Instantiate AcceptorSettingsHelper
 	$acceptor_settings_helper = new AcceptorSettingsHelper( $acceptor_settings );
 
+	if ( $acceptor_settings_helper->create_missing_categories() ) {
+		create_missing_categories( & $posts, $categories, $acceptor_settings_helper );
+	}
+
 	foreach ( $posts as $post_idx => $post ) {
 		$post_status = 'publish';
 
 		if ( ! $acceptor_settings_helper->allow_duplicate_post_title() ) {
-
 			$query = $wpdb->prepare(
 				"SELECT ID, post_title, post_status FROM $wpdb->posts WHERE post_title = %s AND post_type = %s AND post_status = 'publish' LIMIT 1",
 				$post->title,
@@ -52,47 +55,7 @@ foreach( $settings['acceptors'] as $acceptor_sitename => $acceptor_settings ) {
 		}
 
 		if ( $acceptor_settings_helper->create_missing_categories() ) {
-
-			// TODO: Есть смысл вынести создание несуществующих рубрик до всего цикла обхода постов
-			foreach ( $post->categories as $donor_category_slug => $donor_category_name ) {
-				$should_create_category = true;
-				$acceptor_category_id   = $acceptor_settings_helper->default_category_id();
-
-				foreach ( $categories as $category_id => $categories_value ) {
-					$category_find = false;
-
-					switch ( $acceptor_settings_helper->compare_category_by() ) {
-						case 'slug':
-							$category_find = $categories_value['slug'] == $donor_category_slug;
-							break;
-
-						case 'name':
-							$category_find = $categories_value['name'] == $donor_category_name;
-							break;
-					}
-
-					if ( $category_find ) {
-						$should_create_category = false;
-						$acceptor_category_id   = $category_id;
-						continue;
-					}
-				}
-
-				if ( $should_create_category ) {
-					$insert_category_result = wp_insert_term( $donor_category_name, 'category', array( 'slug' => $donor_category_slug ) );
-
-					if ( is_wp_error( $insert_category_result ) ) {
-						$post_category = $insert_category_result->error_data['term_exists'];
-					} else {
-						$post_category = $insert_category_result['term_id'];
-					}
-
-					$post_category = array( $post_category );
-				} else {
-					$post_category = array( $acceptor_category_id );
-				}
-			}
-
+			$post_category = $post->mapped_acceptor_categories;
 		} else {
 			$post_category = array( $acceptor_settings_helper->default_category_id() );
 		}
@@ -111,3 +74,60 @@ foreach( $settings['acceptors'] as $acceptor_sitename => $acceptor_settings ) {
 	}
 }
 
+/**
+ * Creates missing categories
+ * This function will modify the array $posts and add 'mapped_acceptor_categories' property
+ *
+ * @since 0.1
+ * @param array $posts
+ * @param array $categories
+ * @param AcceptorSettingsHelper $acceptor_settings_helper
+ * @return void
+ */
+function create_missing_categories( $posts, $categories, $acceptor_settings_helper ) {
+	foreach ( $posts as $post_idx => $post ) {
+		$all_post_categories = array();
+
+		foreach ( $post->categories as $donor_category_slug => $donor_category_name ) {
+			$should_create_category = true;
+			$acceptor_category_id   = $acceptor_settings_helper->default_category_id();
+
+			foreach ( $categories as $category_id => $categories_value ) {
+				$category_find = false;
+
+				switch ( $acceptor_settings_helper->compare_category_by() ) {
+					case 'slug':
+						$category_find = $categories_value['slug'] == $donor_category_slug;
+						break;
+
+					case 'name':
+						$category_find = $categories_value['name'] == $donor_category_name;
+						break;
+				}
+
+				if ( $category_find ) {
+					$should_create_category = false;
+					$acceptor_category_id   = $category_id;
+					continue;
+				}
+			}
+
+			if ( $should_create_category ) {
+				$insert_category_result = wp_insert_term( $donor_category_name, 'category', array( 'slug' => $donor_category_slug ) );
+
+				if ( is_wp_error( $insert_category_result ) ) {
+					$post_category = $insert_category_result->error_data['term_exists'];
+				} else {
+					$post_category = $insert_category_result['term_id'];
+				}
+
+			} else {
+				$post_category = $acceptor_category_id;
+			}
+
+			$all_post_categories[] = $post_category;
+		}
+
+		$post->mapped_acceptor_categories = array_unique($all_post_categories);
+	}
+}
