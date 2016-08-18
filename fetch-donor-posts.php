f07c64cd74a2ac614bef6186b8d0e407060ea6e6
Alexander Gruzov <gruz0.mail@gmail.com>
2016-08-18 21:47:44 +0300

    Update fetcher! :grin:
    modified:   fetch-donor-posts.php

--- fetch-donor-posts.php
+++ fetch-donor-posts.php
@@ -20,12 +20,21 @@ if ( ! is_dir( APP_PATH . 'posts' ) ) {
 
 include_once( $settings['donor_path'] . 'wp-blog-header.php' );
 
+// Remove previous files from current date
+array_map( 'unlink', glob( "posts/posts-" . date("Ymd") . "*.json" ) );
+
 // TODO: Получение и добавление меток
-// TODO: Загрузка изображений и featured image
+// TODO: Загрузка изображений
 
-$newer_post_date_from_file = load_newer_post_date_from_file();
+// TODO: Здесь надо бы добавить выборку максимального количества постов из настроек WP
+$posts_per_page = 50;
 
-$date_parts = DateTime::createFromFormat( 'Y.m.d H:i:s', $newer_post_date_from_file );
+if ( $settings['first_run'] ) {
+	$date_parts = DateTime::createFromFormat( 'Y.m.d H:i:s', $settings['donor_start_from'] );
+} else {
+	$newer_post_date_from_file = load_newer_post_date_from_file();
+	$date_parts = DateTime::createFromFormat( 'Y.m.d H:i:s', $newer_post_date_from_file );
+}
 
 $args = array(
 	'post_type'  => 'post',
@@ -33,57 +42,106 @@ $args = array(
 	'order'      => 'ASC',
 	'date_query' => array(
 		array(
-			array( 'year'   => (int) $date_parts->format( 'Y' ), 'compare' => '>' ),
-			array( 'month'  => (int) $date_parts->format( 'm' ), 'compare' => '>' ),
-			array( 'day'    => (int) $date_parts->format( 'd' ), 'compare' => '>' ),
-			array( 'hour'   => (int) $date_parts->format( 'H' ), 'compare' => '>' ),
-			array( 'minute' => (int) $date_parts->format( 'i' ), 'compare' => '>' ),
-			array( 'second' => (int) $date_parts->format( 's' ), 'compare' => '>' ),
+			array( 'year'   => (int) $date_parts->format( 'Y' ), 'compare' => '>=' ),
+			array( 'month'  => (int) $date_parts->format( 'm' ), 'compare' => '>=' ),
+			array( 'day'    => (int) $date_parts->format( 'd' ), 'compare' => '>=' ),
+			array( 'hour'   => (int) $date_parts->format( 'H' ), 'compare' => '>=' ),
+			array( 'minute' => (int) $date_parts->format( 'i' ), 'compare' => '>=' ),
+			array( 'second' => (int) $date_parts->format( 's' ), 'compare' => '>=' ),
 		),
 	),
-	'posts_per_page' => -1,
+	'posts_per_page'         => $posts_per_page,
+	'offset'                 => 0,
+	'cache_results'          => false,
+	'update_post_meta_cache' => false,
 );
 
-$posts = array();
-$posts_dates = array();
+$posts                 = array();
+$posts_dates           = array();
+$temp_posts_dates      = array();
+$total_posts_processed = 0;
+$page_number           = 0;
 
-$donor = new WP_Query( $args );
+$donor       = new WP_Query( $args );
+$count_posts = $donor->found_posts;
+$pages_count = (int) ( $count_posts / $posts_per_page );
 
-if ( $donor->have_posts() ) {
-	while ( $donor->have_posts() ) {
-		$donor->the_post();
-
-		$featured_image = '';
-		$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
-		if ( ! empty( $large_image_url[0] ) ) {
-			$featured_image = esc_url( $large_image_url[0] );
-		}
-
-		$categories = get_the_category();
-		$categories_list = array();
-		foreach ( $categories as $category ) {
-			$categories_list[esc_attr( $category->slug )] = esc_attr( $category->name );
-		}
-		$categories = $categories_list;
-
-		$post_date = get_the_date( 'Y.m.d H:i:s' );
-
-		$posts[] = array(
-			'ID'             => get_the_ID(),
-			'title'          => wp_strip_all_tags( get_the_title() ),
-			'content'        => get_the_content(),
-			'date'           => $post_date,
-			'featured_image' => $featured_image,
-			'categories'     => $categories,
-		);
-
-		$posts_dates[] = $post_date;
-	}
-} else {
-	// no posts found
+if ( ( $count_posts % $posts_per_page ) > 0 ) {
+	$pages_count++;
 }
 
-// Store newer post date or use previous post date from file if posts not found
+for ( $idx = 0; $idx < $pages_count; $idx++ ) {
+	$donor = new WP_Query( $args );
+
+	if ( $donor->have_posts() ) {
+		while ( $donor->have_posts() ) {
+			$donor->the_post();
+
+			// Загружаем миниатюру записи
+			$featured_image = '';
+			$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
+			if ( ! empty( $large_image_url[0] ) ) {
+				$featured_image = esc_url( $large_image_url[0] );
+			}
+
+			// Загружаем рубрики
+			$categories = get_the_category();
+			$categories_list = array();
+			foreach ( $categories as $category ) {
+				$categories_list[esc_attr( $category->slug )] = base64_encode( esc_attr( $category->name ) );
+			}
+			$categories = $categories_list;
+
+			// Формируем массив постов для сохранения в виде JSON
+			$post_id    = get_the_ID();
+			$post_title = get_the_title();
+			$post_date  = get_the_date( 'Y.m.d H:i:s' );
+
+			$posts[] = array(
+				'ID'             => $post_id,
+				'title'          => base64_encode( $post_title ),
+				'content'        => base64_encode( get_the_content() ),
+				'date'           => $post_date,
+				'featured_image' => base64_encode( $featured_image ),
+				'categories'     => $categories,
+			);
+
+			echo "Post #{$post_id} \"{$post_title}\" processed – {$post_date}\n";
+
+			$temp_posts_dates[] = $post_date;
+			$total_posts_processed++;
+		}
+
+		if ( count( $posts ) == 50 || ( $idx == ( $pages_count - 1 ) ) ) {
+			file_put_contents( APP_PATH . 'posts/posts-' . date("Ymd") . '-' . sprintf( "%03d", ++$page_number ) . '.json', json_encode( $posts ) );
+
+			// Store newer post date
+			arsort( $temp_posts_dates );
+			$posts_dates[] = array_shift( $temp_posts_dates );
+
+			$posts            = array();
+			$temp_posts_dates = array();
+		}
+
+		echo "===========================\n";
+		echo "Processed now: {$total_posts_processed} of {$count_posts}\n\n";
+
+		$args['offset'] += $posts_per_page;
+		sleep(2);
+
+	// No posts found
+	} else {
+		var_dump( "============ NO POSTS FOUND =============" );
+		break;
+	}
+
+	unset($donor);
+}
+
+echo "===============\n";
+echo "Processed: {$total_posts_processed}\n";
+
+// Store newer post date or use previous post date from file if posts are not found
 if ( count( $posts_dates ) ) {
 	arsort( $posts_dates );
 	$newer_post_date = array_shift( $posts_dates );
@@ -91,11 +149,5 @@ if ( count( $posts_dates ) ) {
 	$newer_post_date = $newer_post_date_from_file;
 }
 
-if ( count( $posts ) ) {
-	file_put_contents( APP_PATH . 'posts/posts-' . date("Ymd") . '.json', json_encode( $posts ) );
-	file_put_contents( NEWER_POST_DATE_FILE_PATH, $newer_post_date );
-	exit(0);
-} else {
-	exit(127);
-}
+file_put_contents( NEWER_POST_DATE_FILE_PATH, $newer_post_date );
 
