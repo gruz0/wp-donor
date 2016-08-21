3147195230aa4cdc4db11636565fb7e6925690f8
Alexander Gruzov <gruz0.mail@gmail.com>
2016-08-21 15:00:18 +0300

    Refactor publish-posts.php
    	modified:   publish-posts.php

--- publish-posts.php
+++ publish-posts.php
@@ -1,7 +1,16 @@
 #!/usr/bin/php
 <?php
-error_reporting(E_ALL);
-set_time_limit(0);
+/**
+ * Publish posts
+ *
+ * @package  WP_Donor
+ * @author   Alexander Gruzov
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/gruz0/wp-donor
+ */
+
+error_reporting( E_ALL );
+set_time_limit( 0 );
 
 define( 'APP_PATH', dirname( __FILE__ ) . DIRECTORY_SEPARATOR );
 define( 'WP_USE_THEMES', false );
@@ -13,12 +22,12 @@ require_once( APP_PATH . 'acceptor-settings-helper.php' );
 $files = get_files();
 
 for ( $idx = 0; $idx < count( $files ); $idx++ ) {
-	echo "\nProcessing file posts/{$files[$idx]}...\n";
+	echo "\n" . 'Processing file posts/' . $files[ $idx ] . "...\n";
 
-	$content = file_get_contents( APP_PATH . 'posts/' . $files[$idx] );
+	$content = file_get_contents( APP_PATH . 'posts/' . $files[ $idx ] );
 	$posts = json_decode( $content );
 
-	foreach( $settings['acceptors'] as $acceptor_sitename => $acceptor_settings ) {
+	foreach ( $settings['acceptors'] as $acceptor_sitename => $acceptor_settings ) {
 		require_once( $acceptor_settings['path'] . 'wp-load.php' );
 
 		// Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
@@ -26,20 +35,20 @@ for ( $idx = 0; $idx < count( $files ); $idx++ ) {
 
 		$wp_upload_dir = wp_upload_dir();
 
-		// Remove filters to use raw data when inserting the post
+		// Remove filters to use raw data when inserting the post.
 		kses_remove_filters();
 
-		// Retrieve all categories
+		// Retrieve all categories.
 		$all_categories = get_categories( 'hide_empty=0' );
 		$categories = array();
 		foreach ( $all_categories as $category ) {
-			$categories[$category->cat_ID] = array(
+			$categories[ $category->cat_ID ] = array(
 				'slug' => esc_attr( $category->slug ),
 				'name' => esc_attr( $category->name ),
 			);
 		}
 
-		// Instantiate AcceptorSettingsHelper
+		// Instantiate AcceptorSettingsHelper.
 		$acceptor_settings_helper = new AcceptorSettingsHelper( $acceptor_settings );
 
 		if ( $acceptor_settings_helper->create_missing_categories() ) {
@@ -82,11 +91,11 @@ for ( $idx = 0; $idx < count( $files ); $idx++ ) {
 				$post_category = array( $acceptor_settings_helper->default_category_id() );
 			}
 
-			// NOTE: Если пост не публикуется, то скорее всего по причине пустого контента
+			// NOTE: Если пост не публикуется, то скорее всего по причине пустого контента.
 			$post_content = html_entity_decode( $post->content );
 			$post_content = empty( $post_content ) ? '<p></p>' : $post_content;
 
-			// Create post object
+			// Create post object.
 			$new_post = array(
 				'post_title'    => $post->title,
 				'post_content'  => $post_content,
@@ -95,10 +104,10 @@ for ( $idx = 0; $idx < count( $files ); $idx++ ) {
 				'post_category' => $post_category,
 			);
 
-			echo "Publishing post #{$post->ID} \"{$post->title}\"... ";
+			echo 'Publishing post #' . $post->ID . ' "' . $post->title . '"... ';
 
-			// Insert the post into the database
-			// True in last param used in WP_Error to return error description
+			// Insert the post into the database.
+			// True in last param used in WP_Error to return error description.
 			$result = wp_insert_post( $new_post, true );
 
 			if ( is_wp_error( $result ) ) {
@@ -108,11 +117,11 @@ for ( $idx = 0; $idx < count( $files ); $idx++ ) {
 			} else {
 				$new_post_id = $result;
 
-				echo "Done! New ID: #{$new_post_id}\n";
+				echo 'Done! New ID: #' . $new_post_id . "\n";
 
-				// Try to insert featured image
+				// Try to insert featured image.
 				if ( $acceptor_settings_helper->use_featured_images() ) {
-					echo "Uploading featured image... ";
+					echo 'Uploading featured image... ';
 
 					if ( empty( $post->featured_image ) && $acceptor_settings_helper->skip_featured_image_if_empty() ) {
 						echo "Skip because it empty!\n\n";
@@ -129,16 +138,16 @@ for ( $idx = 0; $idx < count( $files ); $idx++ ) {
 
 			$posts_processed++;
 
-			if ( $posts_processed % 25 == 0 || ( $post_idx == ( $posts_count - 1 ) ) ) {
-				echo date_with_timezone("Y-m-d H:i:s") . " – Processed {$posts_processed} posts from file {$files[$idx]}. Sleeping...\n";
-				sleep(2);
+			if ( 0 === $posts_processed % 25 || ( ( $posts_count - 1 ) === $post_idx ) ) {
+				echo date_with_timezone( 'Y-m-d H:i:s' ) . ' – Processed ' . $posts_processed . ' posts from file ' . $files[ $idx ] . " Sleeping...\n";
+				sleep( 2 );
 			}
 		}
 	}
 }
 
 if ( count( $errors ) ) {
-	var_dump( "=== ERRORS ===" );
+	var_dump( '=== ERRORS ===' );
 	var_dump( $errors );
 }
 
