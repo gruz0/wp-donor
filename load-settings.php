6e8f28fb964f2f026482af0cdde57b75c56bdcf0
Alexander Gruzov <gruz0.mail@gmail.com>
2016-08-21 14:57:37 +0300

    Refactor load-settings.php
    	modified:   load-settings.php

--- load-settings.php
+++ load-settings.php
@@ -1,68 +1,77 @@
 <?php
+/**
+ * Load settings from settings.php and validate it
+ *
+ * @package  WP_Donor
+ * @author   Alexander Gruzov
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/gruz0/wp-donor
+ */
 
 if ( ! file_exists( APP_PATH . 'settings.php' ) || ! is_readable( APP_PATH . 'settings.php' ) ) {
 	echo 'settings.php is not exists or readable';
-	die(1);
+	die( 1 );
 }
 
 require_once( APP_PATH . 'settings.php' );
 
 if ( empty( $settings ) || ! is_array( $settings ) ) {
 	echo 'Settings are not present!';
-	die(1);
+	die( 1 );
 }
 
 if ( empty( $settings['donor_path'] ) || ! is_dir( $settings['donor_path'] ) || ! is_readable( $settings['donor_path'] ) ) {
 	echo 'donor_path is not present in settings or not exists or not readable';
-	die(1);
+	die( 1 );
 }
 
 if ( ! empty( $settings['first_run'] ) && $settings['first_run'] ) {
 	if ( empty( $settings['donor_start_from'] ) ) {
 		echo 'donor_start_from is not present in settings';
-		die(1);
+		die( 1 );
 	}
 
 	if ( DateTime::createFromFormat( 'Y.m.d', $settings['donor_start_from'] ) ) {
 		$settings['donor_start_from'] .= ' 00:00:00';
 
 	} elseif ( DateTime::createFromFormat( 'Y.m.d H:i:s', $settings['donor_start_from'] ) ) {
+		// Nothing here.
 
 	} else {
 		echo 'donor_start_from has not a valid format. Use "Y.m.d H:i:s"';
-		die(1);
+		die( 1 );
 	}
 } else {
 	$settings['first_run'] = false;
 }
 
-if ( empty( $settings['acceptors'] ) || ! is_array( $settings['acceptors'] ) || count( $settings['acceptors'] ) == 0 ) {
+if ( empty( $settings['acceptors'] ) || ! is_array( $settings['acceptors'] ) || count( $settings['acceptors'] ) === 0 ) {
 	echo 'acceptors are not present in settings or not an array';
-	die(1);
+	die( 1 );
 }
 
-foreach( $settings['acceptors'] as $acceptor_name => $acceptor_values ) {
+foreach ( $settings['acceptors'] as $acceptor_name => $acceptor_values ) {
 	if ( empty( $acceptor_name ) ) {
 		echo 'acceptor acceptor_name is empty';
-		die(1);
+		die( 1 );
 	}
 
-	if ( empty( $acceptor_values ) || ! is_array( $acceptor_values ) || count( $acceptor_values ) == 0 ) {
+	if ( empty( $acceptor_values ) || ! is_array( $acceptor_values ) || count( $acceptor_values ) === 0 ) {
 		echo 'acceptors are not present in settings or not an array';
-		die(1);
+		die( 1 );
 	}
 
-	foreach( $acceptor_values as $key => & $value ) {
+	foreach ( $acceptor_values as $key => & $value ) {
 		if ( ! is_string( $key ) ) {
 			echo 'acceptor key is not present a string';
-			die(1);
+			die( 1 );
 		}
 
 		switch ( $key ) {
 			case 'path': {
 				if ( empty( $value ) || ! is_dir( $value ) || ! is_readable( $value ) ) {
 					echo 'acceptor path is not present or not exists or not readable';
-					die(1);
+					die( 1 );
 				}
 				break;
 			}
@@ -70,7 +79,7 @@ foreach( $settings['acceptors'] as $acceptor_name => $acceptor_values ) {
 			case 'start_from': {
 				if ( empty( $value ) ) {
 					echo 'acceptor start_from is empty';
-					die(1);
+					die( 1 );
 				}
 
 				if ( DateTime::createFromFormat( 'Y.m.d', $value ) ) {
@@ -80,7 +89,7 @@ foreach( $settings['acceptors'] as $acceptor_name => $acceptor_values ) {
 
 				} else {
 					echo 'acceptor start_from has not a valid format. Use "Y.m.d H:i:s"';
-					die(1);
+					die( 1 );
 				}
 
 				break;
@@ -88,7 +97,7 @@ foreach( $settings['acceptors'] as $acceptor_name => $acceptor_values ) {
 		}
 	}
 
-	// If start_from does not present then we use default value '1970.01.01 00:00:00'
+	// If start_from does not present then we use default value '1970.01.01 00:00:00'.
 	if ( empty( $acceptor_values['start_from'] ) ) {
 		$acceptor_values['start_from'] = '1970.01.01 00:00:00';
 	}
